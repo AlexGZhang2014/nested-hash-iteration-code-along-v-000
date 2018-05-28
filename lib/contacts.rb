3a5fef40e2def88de6ab76dea720dcc7b3aace54
Alex Zhang <alexgzhang92@gmail.com>
2018-05-28 01:04:41 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,11 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  
+  contacts.each do |name, items|
+    items.each do |key, value|
+      if key == "favorite_ice_cream_flavors"
+        value.
+      end
+    end
+  end
 end
